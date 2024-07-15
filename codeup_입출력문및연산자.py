f98a8323ab46b12547721bbfec1afc5add57678f
wisdomsy <157783196+wisdomsy@users.noreply.github.com>
2024-07-15 15:25:59 +0900

    Add files via upload
    codeup_입출력문자및연산자

---
+++ codeup_입출력문및연산자.py
@@ -0,0 +1,52 @@
+#1125
+# a=int(input())
+# print('%o'%a,'%X'%a)
+
+#1126
+# a=input()
+# print(a)
+
+# #1135
+# a,b=map(int,input().split())
+# if a>=b:
+#     print(1)
+# else:
+#     print(0)
+
+#1136
+# a,b=map(int,input().split())
+# if a==b:
+#     print(1)
+# else:
+#     print(0)
+
+#1137
+# a,b=map(int,input().split())
+# if a!=b:
+#     print(1)
+# else:
+#     print(0)
+
+#1138
+# a,b=map(int,input().split())
+# print(a<<b)
+
+#1149
+# a,b=map(int,input().split())
+# if a>=b:
+#     print(a)
+# else:
+#     print(b)
+
+#1150
+# a,b,c=map(int,input().split())
+# if a>=b:
+#     if b>=c:
+#         print(c)
+#     else:
+#         print(b)
+# else:
+#     if a>=c:
+#         print(c)
+#     else:
+#         print(a)
